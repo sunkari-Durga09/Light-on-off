8d8126e9eeb2f2aeb111577d402d4476766dd966
SUNKARI Durga <durga16092006@gmail.com>
2026-02-12 15:37:22 +0530

    Create E.js

---
+++ E.js
@@ -0,0 +1,5 @@
+function lightOn() {
+    document.getElementById("bulb").src =
+        "https://www.w3schools.com/js/pic_bulbon.gif";
+    document.getElementById("body").style.backgroundColor = "#fff8b3";
+}
